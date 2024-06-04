d4dc14422ec83e7f52842c3ac609e3d3ed306b88
Jusypablo13 <jusypablolopezr@gmail.com>
2024-06-03 18:23:18 -0600

    New main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "MyGame.h"
 #include <cstdlib>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
+    cout << "Number of arguments: " << argc << endl;
+    for (int i = 0; i < argc; ++i) {
+        cout << "Argument " << i << ": " << argv[i] << endl;
+    }
+
     if (argc != 9) {
-        cout << "Usage: " << argv[0] << numTiles << numSnakes<< numLadders<< penalty<< reward<< numPlayers<< maxTurns<< gameType<< endl;
+        cout << "Usage: " << argv[0] << " <numTiles> <numSnakes> <numLadders> <penalty> <reward> <numPlayers> <maxTurns> <gameType>" << endl;
         return 1;
     }
 
@@ -14,7 +22,16 @@ int main(int argc, char* argv[]) {
     int reward = atoi(argv[5]);
     int numPlayers = atoi(argv[6]);
     int maxTurns = atoi(argv[7]);
-    char gameType = argv[8][1];
+    char gameType = argv[8][0];
+
+    cout << "numTiles: " << numTiles << endl;
+    cout << "numSnakes: " << numSnakes << endl;
+    cout << "numLadders: " << numLadders << endl;
+    cout << "penalty: " << penalty << endl;
+    cout << "reward: " << reward << endl;
+    cout << "numPlayers: " << numPlayers << endl;
+    cout << "maxTurns: " << maxTurns << endl;
+    cout << "gameType: " << gameType << endl;
 
     MyGame game(numTiles, numSnakes, numLadders, penalty, reward, numPlayers, maxTurns, gameType);
     game.start();
